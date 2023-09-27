76f0abe9bf0c0d7f608e6bbcc58b08b3201842b0
NguyenNam6689 <nhnam.12c2nvtroi@gmail.com>
2023-09-28 00:54:13 +0700

    #2 [ [ useEffect ] update learn 1

--- src/components/useEffect/learn_1/learn_1.js
+++ src/components/useEffect/learn_1/learn_1.js
@@ -6,19 +6,22 @@ const LearnApi = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [activeButton, setActiveButton] = useState(null);
+  const [error, setError] = useState(null);
+  const [search, setSearch] = useState('');
+  const [pr, setPr] = useState('name'); // Initialize pr with 'name'
 
   const handleFetchApi = (param) => {
     setLoading(true);
-    setActiveButton(param); // Đặt nút hiện tại thành nút được bấm
+    setActiveButton(param);
     return axios
       .get(`https://jsonplaceholder.typicode.com/${param}`)
       .then((rs) => {
         setData(rs.data);
         setLoading(false);
       })
-      .catch((err) => {
+      .catch((error) => {
         setLoading(false);
-        console.error(err);
+        setError(error);
       });
   };
 
@@ -26,42 +29,85 @@ const LearnApi = () => {
     handleFetchApi('users');
   }, []);
 
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleClear = () => {
+    setSearch('');
+  };
+
+  const filteredData = data.filter((item) => {
+    const property = item[pr];
+    return property && property.toLowerCase().includes(search.toLowerCase());
+  });
+
+  const handleChangeParam = (param) => {
+    if (param === 'users' || param === 'comments') {
+      setPr('name');
+    } else if (param === 'posts' || param === 'albums') {
+      setPr('title');
+    }
+  };
+
   return (
     <>
       <div className="button-container">
+        <input type="text" value={search} onChange={handleChange} placeholder="Search by name" />
+        <button onClick={handleClear}>Clear</button>
         <button
-          onClick={() => handleFetchApi('users')}
+          onClick={() => {
+            handleFetchApi('users');
+            handleChangeParam('users');
+          }}
           className={activeButton === 'users' ? 'active-button' : ''}
         >
           Users
         </button>
         <button
-          onClick={() => handleFetchApi('comments')}
+          onClick={() => {
+            handleFetchApi('comments');
+            handleChangeParam('comments');
+          }}
           className={activeButton === 'comments' ? 'active-button' : ''}
         >
           Comments
         </button>
         <button
-          onClick={() => handleFetchApi('posts')}
+          onClick={() => {
+            handleFetchApi('posts');
+            handleChangeParam('posts');
+          }}
           className={activeButton === 'posts' ? 'active-button' : ''}
         >
           Posts
         </button>
         <button
-          onClick={() => handleFetchApi('albums')}
+          onClick={() => {
+            handleFetchApi('albums');
+            handleChangeParam('albums');
+          }}
           className={activeButton === 'albums' ? 'active-button' : ''}
         >
-          Posts
+          Albums
         </button>
       </div>
       {loading ? (
-        <h1>...Loading</h1>
+        <h1>Loading...</h1>
       ) : (
-        <ul>
-          {data.map((item) => (
-            <li key={item.id}>{item.name || item.body || item.title}</li>
-          ))}
-        </ul>
+        <>
+          {error ? (
+            <h1>Error: {error.message}</h1>
+          ) : (
+            <>
+              <ul>
+                {filteredData.map((item) => (
+                  <li key={item.id}>{item[pr]}</li>
+                ))}
+              </ul>
+            </>
+          )}
+        </>
       )}
     </>
   );
