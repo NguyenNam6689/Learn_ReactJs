62fde8f2a950bc089d9d4feec7cf31a64c5cb99a
NguyenNam6689 <nhnam.12c2nvtroi@gmail.com>
2023-10-01 20:26:40 +0700

    #2 [ useEffect ] update learn_2

--- src/components/useEffect/hooks.js
+++ src/components/useEffect/hooks.js
@@ -1,13 +1,13 @@
 import React, { Fragment } from 'react';
 import LearnApi from './learn_1/learn_1.js';
-// import ScrollProgress from './learn_2/learn_2.js';
+import ScrollProgress from './learn_2/learn_2.js';
 import SearchApi from './learn_exam/learn_exam.js';
 
 const Hook = () => {
   return (
     <Fragment>
-      <LearnApi />
-      {/* <ScrollProgress /> */}
+      {/* <LearnApi /> */}
+      <ScrollProgress />
       {/* <SearchApi /> */}
     </Fragment>
   );
