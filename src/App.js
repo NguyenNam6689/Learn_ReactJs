5c48f4a590eec6aac243a53f00c0a7d48291041f
NguyenNam6689 <nhnam.12c2nvtroi@gmail.com>
2023-10-05 23:36:44 +0700

    #3 [ Routers ] uupdate useRouters

--- src/App.js
+++ src/App.js
@@ -12,30 +12,31 @@ import Home from './pages/home/Home';
 import LearnApi from './components/useEffect/learn_1/learn_1';
 import ScrollProgress from './components/useEffect/learn_2/learn_2';
 import SearchApi from './components/useEffect/learn_exam/learn_exam';
+import Routers from './routers';
 
 function App() {
   return (
     <>
       {/* <Hook /> */}
       <div>
-        <Navbar />
-        <div>
-          <Routes>
-            {/* Home */}
-            <Route path="/" element={<Home />} />
-            {/* useState */}
-            <Route path="/usestate" element={<UseStateLayout />} />
-            {/* useEffects  */}
-            <Route path="/useeffects">
+        <Routers />
+        {/* <div>
+          <Routes> */}
+        {/* Home */}
+        {/* <Route path="/" element={<Home />} /> */}
+        {/* useState */}
+        {/* <Route path="/usestate" element={<UseStateLayout />} /> */}
+        {/* useEffects  */}
+        {/* <Route path="/useeffects">
               <Route index element={<UseEffectLayout />} />
               <Route path="learn1" element={<LearnApi />} />
               <Route path="learn2" element={<ScrollProgress />} />
               <Route path="learn_exam" element={<SearchApi />} />
-            </Route>
-            {/* NotFound  */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
+            </Route> */}
+        {/* NotFound  */}
+        {/* <Route path="*" element={<NotFound />} />
+          </Routes> */}
+        {/* </div> */}
       </div>
     </>
   );
---
+++ src/routers/index.js
@@ -0,0 +1,57 @@
+import Navbar from 'src/components/Navbar';
+import LearnApi from 'src/components/useEffect/learn_1/learn_1';
+import ScrollProgress from 'src/components/useEffect/learn_2/learn_2';
+import SearchApi from 'src/components/useEffect/learn_exam/learn_exam';
+import NotFound from 'src/pages/NotFound/NotFound';
+import Home from 'src/pages/home/Home';
+import UseEffectLayout from 'src/pages/useEffects/UseEffectLayout';
+import UseStateLayout from 'src/pages/useStates/UseStateLayout';
+import React from 'react';
+import { useRoutes } from 'react-router-dom';
+
+const Routers = () => {
+  let routers = useRoutes([
+    {
+      element: <Navbar />,
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+          index: true,
+        },
+        {
+          path: '/usestate',
+          element: <UseStateLayout />,
+        },
+        {
+          path: '/useeffects',
+          children: [
+            {
+              index: true,
+              element: <UseEffectLayout />,
+            },
+            {
+              path: 'learn1',
+              element: <LearnApi />,
+            },
+            {
+              path: 'learn2',
+              element: <ScrollProgress />,
+            },
+            {
+              path: 'learn_exam',
+              element: <SearchApi />,
+            },
+          ],
+        },
+        {
+          path: '*',
+          element: <NotFound />,
+        },
+      ],
+    },
+  ]);
+  return routers;
+};
+
+export default Routers;
